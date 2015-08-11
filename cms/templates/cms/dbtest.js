5d914d614ccf4b0c6a622af81d0bcbfa55673495
Ryuta-Abe <abe@mobcom.ait.kyushu-u.ac.jp>
2015-08-11 23:34:05 +0900

    remove cache

--- cms/templates/cms/dbtest.js
+++
@@ -1,5 +0,0 @@
-var db = new Mongo().getDB( "sensordb" );
-var col = db.getCollection( "sensor2" );
-var data = col.find().sort( { device_id:40add53c } );
-print( "User Agent Summary \n" );
-data.forEach( function( data ) { print( "Agent = " + data.device_id ); } );
