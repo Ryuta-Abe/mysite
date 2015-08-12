b6ef421b7739814ba51fa6255a80faef16077b25
Ryuta-Abe <abe@mobcom.ait.kyushu-u.ac.jp>
2015-08-12 17:04:03 +0900

    positionset(db)周りの不具合修正

--- cms/views.py
+++ cms/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
 from cms.forms import SensorForm
-from cms.models import Sensor2, Sensor3, initial_db, temp_db, error_db,positionset
+from cms.models import Sensor2, Sensor3, initial_db, temp_db, error_db, positionset
 from mongoengine import *
 from pymongo import *
 import requests
