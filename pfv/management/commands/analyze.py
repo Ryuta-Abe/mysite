f465a7ac86ea014d71cd30a47f5a93f5bcb2e2a0
Ryuta-Abe <abe@mobcom.ait.kyushu-u.ac.jp>
2016-10-02 19:14:16 +0900

    旧実行方法も使えるよう変更

--- pfv/management/commands/analyze.py
+++ pfv/management/commands/analyze.py
@@ -1,34 +1,54 @@
 # -*- coding: utf-8 -*-
+import time
+all_st = time.time()
 from django.core.management.base import BaseCommand
 
-from pfv.scripts.convert_datetime import *
-from pfv.scripts.aggregate import *
-from pfv.scripts.get_start_end import get_start_end_mod
+# from pfv.scripts.convert_datetime import *
+# from pfv.scripts.aggregate import *
+# from pfv.scripts.get_start_end import get_start_end_mod
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../../scripts')
+# print(sys.path)
 
+from convert_datetime import *  
+from aggregate import *
+from get_start_end import get_start_end_mod
+from aggregate_raw100 import aggregate_raw100
+
+from pymongo import *
+client = MongoClient()
+db = client.nm4bd
 
 class Command(BaseCommand):
-  args = '<st_dt[int14digits] ed_dt[int14digits]>'
-  help = u'analyze data : aggregate->get_start_end->save'
+    args = '<st_dt[int14digits] ed_dt[int14digits]>'
+    help = u'analyze data : aggregate->get_start_end->save'
 
-  def handle(self, *args, **options):
-    st_dt = str(args[0])
-    ed_dt = str(args[1])
-    
-    datas = db.trtmp.find()
-    for data in datas:
-      data["dt_end05"] = int(dt_end_to_05(data["get_time_no"]))
-      db.trtmp.save(data)
+    def handle(self, *args, **options):
+        st_dt = dt_from_14digits_to_iso(str(args[0]))
+        ed_dt = dt_from_14digits_to_iso(str(args[1]))
+        
+        datas = db.trtmp.find()
+        for data in datas:
+            data["dt_end05"] = data["get_time_no"]
+            db.trtmp.save(data)
 
-    tmp_st = dt_end_to_05(st_dt)
-    tmp_st = dt_from_14digits_to_iso(tmp_st)
-    ed_dt  = dt_from_14digits_to_iso(ed_dt)
+        st_dt  = dt_from_iso_to_str(st_dt)
+        tmp_st = dt_from_14digits_to_iso(st_dt)
+        tmp_ed = dt_from_iso_to_str(ed_dt)
+        ed_dt  = dt_from_14digits_to_iso(tmp_ed)
 
-    while(tmp_st < ed_dt):
-      after_5s = shift_seconds(tmp_st, 5)
-      int_st = int(dt_from_iso_to_str(tmp_st)[:14])
-      int_ed = int(dt_from_iso_to_str(after_5s)[:14])
+        while(tmp_st < ed_dt):
+            loop_st = time.time()
+            after_5s = shift_seconds(tmp_st, 5)
+            iso_st = tmp_st
+            iso_ed = after_5s
 
-      aggregate_mod(int_st, int_ed, False, False, True)
-      get_start_end_mod(False, True)
-      # print(tmp_st)
-      tmp_st = after_5s
+            ### execute following all process ###
+            aggregate_mod(iso_st, iso_ed, False, False, True)
+            aggregate_raw100(iso_ed)
+            get_start_end_mod(iso_st, False, True)
+            tmp_st = after_5s
+            #####################################
+            # print(time.time()-loop_st)
+
+        print("total:"+str(time.time()-all_st))
