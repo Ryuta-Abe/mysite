15d388226405cc7fa8ce880f6229b690ecd02f14
Kento Hayata <k.hayata0212@gmail.com>
2016-10-27 21:26:53 +0900

    解析ルートの整合性評価（正解・不正解）を出力するexamine_route完成

--- pfv/scripts/examine_route.py
+++ pfv/scripts/examine_route.py
@@ -8,6 +8,20 @@ db = client.nm4bd
 # CONST
 MATCH_NODE_THRESHOLD = 10
 UPDATE_INTERVAL = 5
+ANALYZE_LAG = 0
+MAC = "00:11:81:10:01:1c"
+
+def is_correct_node(floor,dt,nodes):
+	analyze_time = shift_seconds(dt,ANALYZE_LAG)
+	analyze_data = {}
+	for node in nodes:
+		analyze_data = db.pfvmacinfo.find_one({"floor":floor,"datetime":analyze_time,"mac":MAC})
+		if analyze_data is not None and analyze_data["route"][-1][1] == node:
+			return True
+		elif db.staymacinfo.find({"floor":floor,"datetime":analyze_time,"mac":MAC,"pcwl_id":node}).count() == 1:
+			return True
+	return False
+
 
 
 def find_closest_nodes(dlist,delta_distance):
@@ -37,15 +51,20 @@ def generate_ideal_nodes(floor,st_node,ed_node,st_dt,ed_dt):
 	delta_distance = 0
 	velocity = 0
 	nodes = []
-	temp_dlist = []
+	tmp_dlist = []
 	dlist = []
+	data = {}
+	is_correct = False
+	correct_count = 0
+	examine_count = 0
+	# [正解数,判定数]
 
 	ideal_one_route = db.idealroute.find_one({"$and": [{"floor" : floor},{"query" : st_node},{"query" : ed_node}]})
 
 	if ideal_one_route["query"][0] != st_node:
-	    temp_dlist = ideal_one_route["dlist"]
-	    for i in range(-1,-len(dlist)-1,-1):
-	        dlist.append({"direction":[tmp_dlist[i]["direction"][1],tmp_dlist[i]["direction"][0]],"distance":temp_dlist[i]["distance"]})
+		tmp_dlist = ideal_one_route["dlist"]
+		for i in range(-1,-len(tmp_dlist)-1,-1):
+			dlist.append({"direction":[tmp_dlist[i]["direction"][1],tmp_dlist[i]["direction"][0]],"distance":tmp_dlist[i]["distance"]})
 	else:
 		dlist =  ideal_one_route["dlist"]
 	total_distance = ideal_one_route["total_distance"]
@@ -56,8 +75,14 @@ def generate_ideal_nodes(floor,st_node,ed_node,st_dt,ed_dt):
 		st_next05_dt = dt_to_end_next05(st_dt,"iso")
 		delta_distance = velocity * (st_next05_dt - st_dt).seconds
 		nodes = find_closest_nodes(dlist,delta_distance)
-		db.examine_route.insert({"datetime":st_next05_dt,"nodes":nodes})
-		print(str(st_next05_dt) + " : " + str(nodes))
+		
+		is_correct = is_correct_node(floor,st_next05_dt,nodes)
+		examine_count += 1
+		if is_correct:
+			correct_count += 1
+		
+		db.examine_route.insert({"datetime":st_next05_dt,"nodes":nodes,"is_correct":is_correct})
+		print(str(st_next05_dt) + " : " + str(nodes) + " , " + str(is_correct))
 	else:
 		st_next05_dt = st_dt
 
@@ -65,21 +90,54 @@ def generate_ideal_nodes(floor,st_node,ed_node,st_dt,ed_dt):
 		delta_distance += velocity * UPDATE_INTERVAL
 		nodes = find_closest_nodes(dlist,delta_distance)
 		st_next05_dt = shift_seconds(st_next05_dt,UPDATE_INTERVAL)
-		db.examine_route.insert({"datetime":st_next05_dt,"nodes":nodes})
-		print(str(st_next05_dt) + " : " + str(nodes))
+
+		is_correct = is_correct_node(floor,st_next05_dt,nodes)
+		examine_count += 1
+		if is_correct:
+			correct_count += 1
+		
+		db.examine_route.insert({"datetime":st_next05_dt,"nodes":nodes,"is_correct":is_correct})
+		print(str(st_next05_dt) + " : " + str(nodes) + " , " + str(is_correct))
+	print("\n")
+	return [correct_count,examine_count]
+
 	
 def examine_route(floor,st_node,ed_node,via_nodes_list,st_dt,ed_dt,via_dts_list):
-	# st_dt = dt_to_end_next05(st_dt,"iso")
-	# ed_dt = dt_to_end_next05(ed_dt,"iso")
+	result = []
+	total_correct_count = 0
+	total_examine_count = 0
+	correct_answer_rate = 0
+
 	via_num = len(via_nodes_list)
 	if via_num == 0:
-		generate_ideal_nodes(floor,st_node,ed_node,st_dt,ed_dt)
+		result = generate_ideal_nodes(floor,st_node,ed_node,st_dt,ed_dt)
+		total_correct_count = result[0]
+		total_examine_count = result[1]
+		correct_answer_rate = total_correct_count / total_examine_count * 100
+		print ("correct answer rate : " + str(correct_answer_rate) + "% "
+		 + "( " + str(total_correct_count) + " / " + str(total_examine_count) + " )")
 
 	else:
-		generate_ideal_nodes(floor,st_node,via_nodes_list[0],st_dt,via_dts_list[0])
+		#print(st_dt,via_dts_list[0])
+		result = generate_ideal_nodes(floor,st_node,via_nodes_list[0],st_dt,via_dts_list[0])
+		total_correct_count = result[0]
+		total_examine_count = result[1]
 		for i in range(via_num - 1):
-			generate_ideal_nodes(floor,via_nodes_list[i],via_nodes_list[i+1],via_dts_list[i],via_dts_list[i+1])
-		generate_ideal_nodes(floor,via_nodes_list[-1],ed_node,via_dts_list[-1],ed_dt)
+			result = generate_ideal_nodes(floor,via_nodes_list[i],via_nodes_list[i+1],via_dts_list[i],via_dts_list[i+1])
+			total_correct_count += result[0]
+			total_examine_count += result[1]
+		result = generate_ideal_nodes(floor,via_nodes_list[-1],ed_node,via_dts_list[-1],ed_dt)
+		total_correct_count += result[0]
+		total_examine_count += result[1]
+		correct_answer_rate = total_correct_count / total_examine_count * 100
+		print ("correct answer rate : " + str(correct_answer_rate) + "% "
+		 + "( " + str(total_correct_count) + " / " + str(total_examine_count) + " )")
+	# examine_route += db.examine_route.find()
+	# for nodes_info in examine_route:
+	# 	if db.pfvmacinfo.find()
+
+
+
 
 if __name__ == '__main__':
 	# param = sys.argv
@@ -94,13 +152,16 @@ if __name__ == '__main__':
 	# for i in len(param[6]):
 	# 	via_dts_list.append(dt_from_14digits_to_iso(common_dt+str(param[6][1])))
 	db.examine_route.remove({})
-	floor = "W2-6F"
+	floor = "W2-7F"
 	st_node = 1
-	ed_node = 16
-	via_nodes_list = [5,8]
-	st_dt = dt_from_14digits_to_iso("20161025102609")
-	ed_dt = dt_from_14digits_to_iso("20161025102812")
-	via_dts_list = [dt_from_14digits_to_iso("20161025102641"),dt_from_14digits_to_iso("20161025102730")]
+	ed_node = 1
+	via_nodes_list = [5,7,9,12,17,21,5]
+	common_dt = str(20161020) # 測定時刻における先頭の共通部分
+	st_dt = dt_from_14digits_to_iso(common_dt + str(125800))
+	ed_dt = dt_from_14digits_to_iso(common_dt + str(130126))
+	via_dts_list = [125827,125854,125925,125942,130010,130041,130058]
+	for i in range(len(via_dts_list)):
+		via_dts_list[i] = dt_from_14digits_to_iso(common_dt + str(via_dts_list[i]))
 	examine_route(floor,st_node,ed_node,via_nodes_list,st_dt,ed_dt,via_dts_list)
 
 # debug用
