38e1d40aa93d961d4b74e08a6e25e4c3cd5e8549
Ryuta-Abe <abe@mobcom.ait.kyushu-u.ac.jp>
2016-12-19 01:42:58 +0900

    実験結果出力用修正

--- pfv/scripts/make_tmpcol.py
+++ pfv/scripts/make_tmpcol.py
@@ -7,23 +7,17 @@ import os
 client = MongoClient()
 db = client.nm4bd
 
-##### hourlytolog 作成手順 #####
-# 0. /home/murakami2/tolog/tolog_2016mmdd.json にあるtologデータ回収
-# 1. mongoimport -d nm4bd -c timeoutlog tolog_2016mmdd.json
-# 2. 以下のいづれかを行う
-#    py manage.py make_hourlylog mmdd（解析したい日付を4桁で）
-#    py manage.py make_hourlylog mmddhhmm mmddhhmm(解析したい日時を8桁で、開始・終了の順に)
-#    iso_st,iso_edを設定後、py manage.py make_hourlylog
+# 1. mongoimport -d nm4bd -c csvtest --headerline --type csv exp_param.csv --drop
+# 2. mongoimport -d nm4bd -c test2 rttmp_yyyymmdd.json
 
-#  mongoimport -d nm4bd -c csvtest --headerline --type csv [CSV_File.csv] (--drop)
+### ("fields.txt"が存在しない場合は3-5を行う)
+### 3.key一覧取得
+###   mongo nm4bd --quiet --eval "for (key in db.hourlytolog.findOne()) print(key)" > dbm_field.txt
+### 4.フィールド一覧ソート
+###   py txt_sort.py fields.txt
+### 5.不要なフィールド(_id等)削除 & datetime先頭に移動
 
-# ("fields.txt"が存在しない場合は3-5を行う)
-# 3.key一覧取得
-#   mongo nm4bd --quiet --eval "for (key in db.hourlytolog.findOne()) print(key)" > dbm_field.txt
-# 4.フィールド一覧ソート
-#   py txt_sort.py fields.txt
-# 5.不要なフィールド(_id等)削除 & datetime先頭に移動
-
+# 6. python make tmpcol
 # 6.mongoexport実行
 #   mongoexport --sort {"datetime":1} -d nm4bd -c dbmlog -o dbm19_1c.csv --csv --fieldFile dbm_field.txt
 
@@ -54,6 +48,7 @@ def make_dbmlog(exp_info):
         # print(gte)
         data_5s = {}
         data_5s["datetime"] = gte
+        exist_flag = False
         for ip_data in ip_list:
             ip = str(ip_data["ip"])
             # print(ip)
@@ -61,24 +56,36 @@ def make_dbmlog(exp_info):
             # print(dbm_data)
             if (dbm_data != None):
                 data_5s[ip_data["log_key"]] = dbm_data["dbm"]
+                exist_flag = True
             else:
-                data_5s[ip_data["log_key"]] = None
+                data_5s[ip_data["log_key"]] = -99
                 
-        db.dbmlog.insert(data_5s)
+        if exist_flag:
+            db.dbmlog.insert(data_5s)
+        else:
+            print("None data : "+str(gte))
         
         gte = shift_seconds(gte,5)
         lt  = shift_seconds(gte,5)
 
 if __name__ == '__main__':
     # common_id_list = ["161207_0", "161208_0"]
-    common_id_list = ["161020_0"]
+    common_id_list = ["161213_0"]
     for common_id in common_id_list:
-        # for id_num in range(1,25):
-        for id_num in range(17,29):
+        # id_list = [73,19]
+        # for id_num in id_list:
+        for id_num in range(67,68):
             id_str = common_id + ("0" + str(id_num))[-2:]
             exp_info = db.csvtest.find_one({"exp_id":id_str})
+            print("\n=== " + id_str + " ===")
+
+            ### floor_numは1桁のみ!  ###
+            floor_num = exp_info["floor"][3:4]
             make_dbmlog(exp_info)
-            # print(exp_info)
+            # # print(exp_info)
+            command = 'mongoexport --sort {"datetime":1} -d nm4bd -c dbmlog -o C:/Users/Ryuta/csv/' + id_str +'.csv --csv --fieldFile C:/Users/Ryuta/dbm_field'+ floor_num +'.txt'
+            # # 9F data
             # command = 'mongoexport --sort {"datetime":1} -d nm4bd -c dbmlog -o C:/Users/Ryuta/csv/' + id_str +'.csv --csv --fieldFile C:/Users/Ryuta/dbm_field9.txt'
-            command = 'mongoexport --sort {"datetime":1} -d nm4bd -c dbmlog -o C:/Users/Ryuta/csv/' + id_str +'.csv --csv --fieldFile C:/Users/Ryuta/dbm_field7.txt'
+            # # 7F data
+            # # command = 'mongoexport --sort {"datetime":1} -d nm4bd -c dbmlog -o C:/Users/Ryuta/csv/' + id_str +'.csv --csv --fieldFile C:/Users/Ryuta/dbm_field7.txt'
             os.system(command)
