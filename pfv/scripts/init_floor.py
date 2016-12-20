86655c17e4a7c6811ce47836c8cba212f2dfa2eb
Ryuta-Abe <abe@mobcom.ait.kyushu-u.ac.jp>
2016-12-20 18:25:08 +0900

    floor情報修正

--- pfv/scripts/init_floor.py
+++ pfv/scripts/init_floor.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 import os
 from make_pcwliplist import *
-from save_pcwlnode import *
+from make_pcwlnode import *
 from save_pcwl_route import *
 
 
 if __name__ == '__main__':
     make_pcwliplist()
-    save_pcwlnode()
+    make_pcwlnode()
     print("Please execute 'python3 ./save_pcwl_route.py'")
     # save_route()
     # os.system("python3 save_pcwl_route.py")
